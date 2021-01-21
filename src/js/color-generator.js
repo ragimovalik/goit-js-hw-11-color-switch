41018f8eeda55561498d372ae81ff16c1bece069
ragimovalik <ragimovalik@gmail.com>
2021-01-21 12:12:59 +0200

    Hw-11-1 - Color switcher added

--- src/js/color-generator.js
+++ src/js/color-generator.js
@@ -1,9 +1,31 @@
-import {colors} from './colors'
-
+import { colors } from './colors';
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-console.log(randomIntegerFromInterval);
-console.log(colors);
+const startBtnEl = document.querySelector('button[data-action="start"]');
+const stopBtnEl = document.querySelector('button[data-action="stop"]');
+const bodyEl = document.body;
+
+startBtnEl.addEventListener('click', colorSwitcher);
+stopBtnEl.addEventListener('click', colorSwitcherOff);
+
+let switcherId = null;
+
+function colorSwitcher() {
+  if (switcherId !== null) {
+    return;
+  } else colorSwitcherOn();
+}
+
+function colorSwitcherOn() {
+  switcherId = setInterval(() => {
+    bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+  }, 1000);
+}
+
+function colorSwitcherOff() {
+  clearInterval(switcherId);
+  switcherId = null;
+}
