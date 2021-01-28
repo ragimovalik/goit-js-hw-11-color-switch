5bd544ae6d0fc064089a4dd436bc2e1362e58518
ragimovalik <ragimovalik@gmail.com>
2021-01-28 16:28:01 +0200

    Updates

--- main.cf17ea9a5a4a8c015efa.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{L1EO:function(n,t,e){},QfWi:function(n,t,e){"use strict";e.r(t);e("L1EO");var c=document.querySelector('button[data-action="start"]'),o=document.querySelector('button[data-action="stop"]');document.body;c.addEventListener("click",(function(){if(null!==u)return;u=setInterval((function(){}),1e3)})),o.addEventListener("click",(function(){clearInterval(u),u=null}));var u=null}},[["QfWi",1]]]);
-//# sourceMappingURL=main.cf17ea9a5a4a8c015efa.js.map
