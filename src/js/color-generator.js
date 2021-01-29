0db5f6b291536ca282c8c03781f2ae8faf11a87f
ragimovalik <ragimovalik@gmail.com>
2021-01-29 16:53:40 +0200

    Refactoring (no more use one of the functions)

--- src/js/color-generator.js
+++ src/js/color-generator.js
@@ -8,16 +8,16 @@ const startBtnEl = document.querySelector('button[data-action="start"]');
 const stopBtnEl = document.querySelector('button[data-action="stop"]');
 const bodyEl = document.body;
 
-startBtnEl.addEventListener('click', colorSwitcher);
+startBtnEl.addEventListener('click', colorSwitcherOn);
 stopBtnEl.addEventListener('click', colorSwitcherOff);
 
 let switcherId = null;
 
-function colorSwitcher() {
-  if (switcherId !== null) {
-    return;
-  } else colorSwitcherOn();
-}
+// function colorSwitcher() {
+//   if (switcherId) {
+//     return;
+//   } else colorSwitcherOn();
+// }
 
 function colorUpdater() {
   bodyEl.style.backgroundColor =
@@ -25,12 +25,16 @@ function colorUpdater() {
 }
 
 function colorSwitcherOn() {
+  startBtnEl.disabled = true;
+
   switcherId = setInterval(() => {
     colorUpdater();
   }, 1000);
 }
 
 function colorSwitcherOff() {
+  startBtnEl.disabled = false;
+
   clearInterval(switcherId);
   switcherId = null;
 }
